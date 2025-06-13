0af7d55243b6a03a1f44cbab44470be2ac8fb869
SaiBabu <Sai Babu>
2025-06-13 16:26:19 +0530

    WAR deployment with Tomcat Docker

--- src/main/java/com/example/demo/UserRegistrationApplication.java
+++ src/main/java/com/example/demo/UserRegistrationApplication.java
@@ -2,6 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer; 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
